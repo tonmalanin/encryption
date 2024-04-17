21c8a989c17e5c1b99efaa068ca82d5884916551
Антон Маланьин <malanin.ao@phystech.edu>
2024-04-17 21:14:37 +0300

    add vernam algorythm

--- src/cipher.py
+++ src/cipher.py
@@ -58,7 +58,16 @@ class Vigenere(Cipher):
 
 class Vernam(Cipher):
     def encrypt(self, text, key):
-        pass
+        it = 0
+        result_text = ""
+        for char in text:
+            if char.isspace():
+                result_text += char
+            else:
+                shift = ord(key[it]) - 33
+                result_text += chr(33 + ((ord(char) - 33) ^ shift) % 94)
+                it = (it + 1) % len(key)
+        return result_text
 
     def decrypt(self, text, key):
-        pass
+        return Vernam.encrypt(self, text, key)
