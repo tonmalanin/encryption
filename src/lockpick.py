5399d88370ea9fa74ee2000b811ea7232a04cb8a
Антон Маланьин <malanin.ao@phystech.edu>
2024-04-30 17:58:04 +0300

    made separate file for constants

--- src/lockpick.py
+++ src/lockpick.py
@@ -1,6 +1,7 @@
+from src import constants
+
+
 class LockPick:
-    spreading = [8.17, 1.49, 2.78, 4.25, 12.70, 2.23, 2.02, 6.09, 6.97, 0.15, 0.77, 4.03, 2.41, 6.75, 7.51, 1.93, 0.1,
-                 5.99, 6.33, 9.06, 2.76, 0.98, 2.36, 0.15, 1.97, 0.07, 0]
 
     def hack(self, text):
         options = []
@@ -26,6 +27,6 @@ class LockPick:
             curr_deviation = 0
             for i in range(27):
                 count[i] = (count[i] / total) * 100
-                curr_deviation += abs(count[i] - self.spreading[i])
+                curr_deviation += abs(count[i] - constants.SPREADING[i])
             deviation.append(curr_deviation)
         return options[deviation.index(min(deviation))]
